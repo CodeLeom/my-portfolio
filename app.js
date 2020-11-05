6241ef542ae5b21911e64b479ff5b77acd4b76e5
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-11-05 15:50:07 +0100

    use prettier to lint frontend facing files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker failed', err));
 }
--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 //install the service worker
-self.addEventListener('install', evt => {
-    console.log('service worker installed');
+self.addEventListener("install", (evt) => {
+  console.log("service worker installed");
 });
 
 //activate service worker
-self.addEventListener('activate', evt => {
-    console.log('service worker activated');
+self.addEventListener("activate", (evt) => {
+  console.log("service worker activated");
 });
 
 //fetch event
-self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+self.addEventListener("fetch", (evt) => {
+  console.log("fetch event", evt);
 });
