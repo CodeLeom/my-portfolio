cadfe63c43ed7c00aeba8cef01680d5657ee22ed
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-11-05 16:21:56 +0100

    optimize contact form and javascript imports
    add validation attributes to form inputs.
    get rid of main.js to use only app.js.
    get rid of firebase database rules as only firestore will be used.
    use a shorter syntax to grab form values in app.js

--- public/app.js
+++ public/app.js
@@ -4,3 +4,12 @@ if ('serviceWorker' in navigator) {
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker failed', err));
 }
+
+const contactForm = document.querySelector('#contact-form');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const submission = Object.fromEntries(new FormData(contactForm));
+
+  console.log(submission);
+});
